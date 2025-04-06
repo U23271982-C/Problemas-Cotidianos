e6124286549c6449393223e3ba0a4a52e1e8444c
isradev <u23271982@utp.edu.pe>
2025-04-06 17:24:40 -0500

    Se guardó clases del primer ejercicio

--- src/main/java/cont/Dinero.java
+++
@@ -1,4 +0,0 @@
-package cont;
-
-public class Dinero {
-}
--- src/main/java/cont/Main.java
+++
@@ -1,7 +0,0 @@
-package cont;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
